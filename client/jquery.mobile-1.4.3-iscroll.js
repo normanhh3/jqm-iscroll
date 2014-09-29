ff7aa723d878cce42c704f1208e0eff8817dec9c
Norman H. Harebottle III <nharebottl@usxpress.com>
2014-09-29 13:55:20 -0400

    Initial commit of working version of jQuery Mobile 1.4.3 compatible iScroll implementation with Meteor demo

---
+++ client/jquery.mobile-1.4.3-iscroll.js
@@ -0,0 +1,396 @@
+"use strict";
+
+function flattenObject(obj, prefix, spacer) {
+    var spacer = (spacer==null?'':spacer);
+    var prefix = (prefix==null?'':prefix);
+
+    return Array.prototype.concat.apply([], 
+        $.map(obj, function(v, k){
+            if($.isPlainObject(v)) {
+                return [flattenObject(v, prefix + spacer + k, spacer)];
+            }
+            return prefix + spacer + k;
+        })
+    );
+};
+
+/**
+    key: "data-foo"
+    value: "true" (a string value to be json parsed)
+    obj: {foo: 1}
+    prefix: "data"
+    spacer: "-"
+
+    e.g this function sets the value true on the foo key
+**/
+function setValueOnKeyUsingPath(key, value, obj, prefix, spacer) {
+    var origObj = obj;
+    var lastSegment = null;
+
+    var items = key.split(spacer);
+    items.shift(); // remove the first items since it is the [prefix] item we don't want or need
+
+    for(var i in items) {
+        var segment = items[i];
+        // the last item in the key-chain is the target property
+        if(i < items.length-1) {
+            if($.isPlainObject(obj[segment])) {
+                obj = obj[segment];
+            } else if (obj[segment] == undefined) {
+                var a = {};
+                obj[segment] = a;
+                obj = a;
+            }
+        }
+        lastSegment = segment;
+    }
+
+    var jsonV = null;
+
+    switch($.type(obj[lastSegment])) {
+        case 'string':
+            jsonV = value;
+            break;
+        default:
+            jsonV = JSON.parse(value);
+            break;
+    }
+
+    obj[lastSegment] = jsonV;
+
+    return origObj;
+};
+
+// debouncing function from John Hann
+// http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+var debounce = function (func, threshold, execAsap) {
+    var timeout;
+
+    return function debounced () {
+      var obj = this, args = arguments;
+      function delayed () {
+          if (!execAsap)
+              func.apply(obj, args);
+          timeout = null;
+      };
+
+      if (timeout)
+          clearTimeout(timeout);
+      else if (execAsap)
+          func.apply(obj, args);
+
+      timeout = setTimeout(delayed, threshold || 100);
+    };
+};
+
+var preventDefault = function(e) { e.preventDefault(); };
+
+
+
+// TODO: Refactor state management into an FSM using Machina.JS
+
+/** iScroll Widget **/
+$.widget('mobile.iscroll', {
+    options: {
+        /** TODO: Need to know why this needs to be false! **/
+        preventDefault: false,
+        
+        /** Enabled to support testing in full browser. **/
+        mouseWheel: true,
+        scrollbars: true,
+
+        // Required to trigger the 'scroll' event see http://iscrolljs.com/#custom-events for details
+        probeType: 1,
+
+        // Options configured below here are specific to this widget and SHOULD NEVER BE ALLOWED to conflict with iscroll options
+        pulltorefresh: {
+            enabled: false, 
+            pulllabel: 'Pull to refresh...', 
+            releaselabel: 'Release to refresh...', 
+            loadinglabel: 'Loading...', 
+            pulledeventtimeout: 60000, /** Default timeout of 1 minute **/
+            timeoutHandle: -1,
+            sensitivity: 15 /** How many pixels have to be pulled down before the release to refresh is triggered. **/
+        },
+        fullscreen: false,
+        infinite: true,
+        infiniteSensitivity: 250, /** When a scrollEnd event occurs, if the bottom of the scroller is <= 75 then we trigger an "infinite" event **/
+        infiniteTimeout: 60000 /** Default timeout of 1 minute **/
+    },
+
+    /** This maintains a reference to the IScroll implementation object. **/
+    scroll: null,
+    
+    _configurePullToRefresh: function() {
+        var $el = $(this.element);
+
+        // Create markup for pulldown  
+        var html = '<div class="iscroll-pullDown"><span class="iscroll-pullDownIcon">&nbsp;</span><span class="iscroll-pullDownLabel"></span></div>';
+        if($el.find('.iscroll-pullDown').length === 0) {
+            if($el.children().length > 0) {
+                $el.children().first().before(html);
+            } else {
+                $el.append(html);
+            }
+
+            $el.find('.iscroll-pullDownLabel').text(this.options.pulltorefresh.pulllabel);
+        }
+
+        this.options.pulltorefresh.pullDownHeight = $('.iscroll-pullDown', this.element).height();
+
+        this.options.startY = parseInt(this.options.pulltorefresh.pullDownHeight) * -1;
+
+        /**
+        Strictly speaking this option is NOT a requirement of the pull to refresh implementation as there are no tap handlers registered!
+
+        //Set this to true to let iScroll emit a custom tap event when the scroll area is clicked/tapped but not scrolled.
+        //This is the suggested way to handle user interaction with clickable elements.
+        **/
+        this.options.tap = true;
+
+        this.options.preventDefaultException = {tagName:/.*/};
+        
+        //this.options.keyBindings = false;
+    },
+
+    _attachHandlersPullToRefresh: function() {
+        var wThis = this;
+
+        this.scroll.on('scroll', function() {
+            var $pullDownEl = $('.iscroll-pullDown', this.element);
+
+            // if there is NOT already a pulldown event in process
+            // if the pulldown distance is greater than the sensitivity setting in pixels
+            // if the location of the pulldown is at the top - indicating a true pull down
+            // if the pullDownEl is a valid reference and is not already in progress
+            // THEN we switch state
+            if(
+                wThis.options.pulltorefresh.timeoutHandle < 0 
+                && this.distY > (wThis.options.pulltorefresh.sensitivity + wThis.options.pulltorefresh.pullDownHeight)
+                && this.y <= wThis.options.pulltorefresh.pullDownHeight 
+                && $pullDownEl 
+                && !$pullDownEl.hasClass('flip')) {
+                    $pullDownEl
+                        .addClass('flip')
+                        .find('.iscroll-pullDownLabel')
+                            .text(wThis.options.pulltorefresh.releaselabel);
+            }
+        });
+
+        this.scroll.on('scrollEnd', function() {
+            // check to see if there is NOT already a refresh in process
+            var $pullDownEl = $('.iscroll-pullDown', this.element);
+
+            if(wThis.options.pulltorefresh.timeoutHandle < 0) {
+                $pullDownEl = $('.iscroll-pullDown', this.element);
+
+                // and we are read to trigger a refresh
+                if($pullDownEl && $pullDownEl.hasClass('flip') && wThis.options.pulltorefresh.timeoutHandle < 0) {
+                    // remove the intermediate up arrow display and replace it with a loading display
+                    $pullDownEl
+                        .removeClass('flip')
+                        .addClass('loading')
+                        .find('.iscroll-pullDownLabel')
+                            .text(this.options.pulltorefresh.loadinglabel)
+
+                    // Reset the top of the scroller
+                    $('.scroller', wThis.element).css({top:0});
+
+                    var d = $.Deferred();
+                    d.always(function() {
+                        window.clearTimeout(wThis.options.pulltorefresh.timeoutHandle);
+                        wThis.options.pulltorefresh.timeoutHandle = -1;
+
+                        $pullDownEl
+                            .removeClass('loading')
+                            .find('.iscroll-pullDownLabel')
+                                .text(wThis.options.pulltorefresh.pulllabel);
+
+                        // Just check to see if the current scroll position is near the top, if so, reset the scroll position to the correct top
+                        if(wThis.scroll.y >= -10) {
+                            wThis._resetScrollPosition();
+                        }
+                    });
+
+                    wThis.options.pulltorefresh.timeoutHandle = wThis._delay(function() { d.resolve(this); }, wThis.options.pulltorefresh.pulledeventtimeout);
+                    wThis._trigger('pulled', null, {deferred: d});
+                } else if (wThis.scroll.y >= (wThis.options.pulltorefresh.pullDownHeight *-1)) {
+                    wThis._resetScrollPosition(200);
+                }
+            }
+        });
+    },
+
+    _resetScrollPosition: function(delay) {
+        this.scroll.scrollTo(0, this.options.pulltorefresh.pullDownHeight * -1, delay == undefined || delay == null ? 750 : delay);
+    },
+
+    _attachHandlersInfiniteLoading: function() {
+        var wThis = this;
+
+        this.scroll.on('scrollEnd', function() {
+            if(this.y < 0 && this.scrollerHeight - (this.y * -1 + this.wrapperHeight) < this.options.infiniteSensitivity) {
+
+                var d = $.Deferred();
+                d.always(function() {
+                    window.clearTimeout(wThis.options.infiniteHandle);
+                    wThis.options.infiniteHandle = -1;
+
+                    wThis.refresh();
+                });
+
+                wThis.options.infiniteHandle = wThis._delay(function() { d.resolve(this); }, wThis.options.infiniteTimeout);
+
+                wThis._trigger('infinite', null, {deferred: d});
+            }
+        });
+    },
+
+    _create: function() {
+        var wThis = this;
+
+        var $el = $(this.element);
+
+        // get a list of attribute values that were set on the element as a way to configure the widget's options
+        // flattenObject(this.options, 'data', '-')
+
+        // TODO: Solve a problem with the case of the attr.localName being in lowercase and the options being set that are mixed case!
+        $.each(
+            $.map($el[0].attributes, function(attr) {
+                if(/^data-/.test(attr.localName)) 
+                    return attr.localName.toLowerCase();
+            }), function(i, propName) {
+                setValueOnKeyUsingPath(propName, $el.attr(propName), wThis.options, 'data', '-');
+            }
+        );
+
+        $(document)
+            /** If this isn't the first iScroll initialized in the app we should unhook the existing handler just so we don't create duplicates! **/
+            .off('touchmove', preventDefault) 
+            /** Turn off the default handling of the touchmove event so that iScroll can take care of it! **/
+            .on('touchmove', preventDefault);
+
+        // don't scroll the whole page, just the content intended to be scrolled!
+        $('body').css('overflow', 'hidden');
+
+        // Step 1) Configure for handling specific options
+        if(this.options.pulltorefresh.enabled) {
+            this._configurePullToRefresh();
+        }
+
+        // Step 2) Create the instance
+
+        // attach the scroller class to the widget element
+        var $sc = $(this.element)
+            .addClass('iscroll-scroller');
+
+        // add a wrapper element as a parent of the widget and assign the wrapper css class
+        var $wr = $sc
+            .wrap(
+                $('<div class="iscroll-wrapper"></div>')
+            )
+            .parents('.iscroll-wrapper')
+            .css({
+                position: 'relative',
+                overflow: 'hidden'
+            });
+
+        if(this.options.fullscreen) {
+            var headerHeight = $('[data-role=header][data-position=fixed]').outerHeight(true);
+            var footerHeight = $('[data-role=footer][data-position=fixed]').outerHeight(true);
+
+            $wr
+                .css({
+                    // setting bottom and top means that the height is managed by the browser
+                    bottom: footerHeight,
+                    top: headerHeight,
+                    position: 'absolute'
+                })
+                .parents('.ui-content')
+                    // Reset default ui-content padding applied by JQM
+                    .css({padding: '0px'});
+
+            if(this.options.pulltorefresh.enabled) {
+                $wr
+                    .find('.iscroll-pullDown')
+                    .css({'margin-bottom': '0px', 'padding-bottom': '0px'});
+            }
+        }
+
+        console.log('creating iscroll instance with options: ', this.options);
+
+        this.scroll = new IScroll($wr[0], this.options);
+
+        // Step 4) Attach handlers
+        if(this.options.pulltorefresh.enabled) {
+            this._attachHandlersPullToRefresh();
+        }
+
+        if(this.options.infinite) {
+            this._attachHandlersInfiniteLoading();
+        }
+
+        // the "this" context for the handlers is the iScroll instance
+        this.scroll.on('beforeScrollStart', function(){ wThis._trigger('beforestart', null, this);  });
+        this.scroll.on('scrollCancel',      function(){ wThis._trigger('cancel', null, this)        });
+        this.scroll.on('scrollStart',       function(){ wThis._trigger('start', null, this);        });
+        this.scroll.on('scroll',            function(){ wThis._trigger('scroll', null, this);       });
+        this.scroll.on('scrollEnd',         function(){ wThis._trigger('end', null, this);          });
+        this.scroll.on('flick',             function(){ wThis._trigger('flick', null, this);        });
+        this.scroll.on('zoomStart',         function(){ wThis._trigger('zoomstart', null, this);    });
+        this.scroll.on('zoomEnd',           function(){ wThis._trigger('zoomEnd', null, this);      });
+
+        // Step 5) In order to prevent seeing the "pull down to refresh" before the iScoll is trigger - 
+        // the wrapper is located at left:-9999px and returned to left:0 after the iScoll is initiated
+        this._delay(function() { 
+            $wr.css({left:0}); 
+            wThis._trigger('created');
+            this.scroll.refresh();
+        }, 300);
+    },
+    
+    _destroy: function() {
+        var $el = $(this.element);
+
+        $el.removeAttr('data-iscroll');
+
+        // if this is the last iscroll on the currently active page remove the body overflow:hidden css setting
+        if($(':mobile-pagecontainer').pagecontainer('getActivePage').find('[data-iscroll]').not($el).length == 0)
+            $('body').css('overflow', 'visible');
+
+        // Unwrap the iscroll-wrapper element from the stack, remove the added iscroll-scroller class
+        $el.unwrap().removeClass('iscroll-scroller');
+
+        if(this.options.fullscreen) {
+            // remove the ui-content class padding reset
+            $el.parents('.ui-content').css({padding: ''});
+        }
+
+        if(this.options.pulltorefresh.enabled) {
+            $el.find('.iscroll-pullDown').remove();
+        }
+
+        $(document)
+            /** If this isn't the first iScroll initialized in the app we should unhook the existing handler just so we don't create duplicates! **/
+            .off('touchmove', preventDefault);
+
+        if(this.scroll !== null)
+            this.scroll.destroy();
+    },
+
+    _refresh: function() {
+        this.scroll.refresh();
+    },
+
+    // See http://iscrolljs.com/#refresh for the full details on the refresh of the iscrollpull
+    refresh: function() {
+        if(this.scroll) {
+            this._delay(this._refresh, 200);
+        }
+    }
+});
+
+// NOTE: pageshow event timing is NOT optional!  If the pageshow event is NOT used the header and footer height are NOT returned appropriately!
+$(document).on('pageshow', function() {
+    $('[data-iscroll=true]').iscroll();
+});
---
+++ index.js
@@ -0,0 +1,76 @@
+if (Meteor.isClient) {
+  Meteor.startup(function() {
+
+    $('#scroller').on('iscrollpulled', function(ev, d) {
+      console.log('making mock asynchronous call');
+
+      window.setTimeout(function() {
+        var $sc = $('#scroller');
+        $sc.children(':eq(1)').children(':eq(0)').before('<p class="bubbledRight">New item added from refresh!</p>');
+        $sc.iscroll('refresh');
+
+        d.deferred.resolve(); 
+      }, 5500);
+    });
+
+    /**
+    $('#scroller').on('create', function() {
+      console.log('create of iscroll happened');
+      // assign bubbled classes
+    });
+
+    $('#scroller').on('iscrollstart', function(ev, d){
+      console.log('iscrollstart ev', ev, 'd', d);
+    });
+    **/
+
+    var recalculateHeight = function($this) {
+      $('p:odd', $this).not('.bubbledLeft').addClass('bubbledLeft');
+      $('p:even', $this).not('.bubbledRight').addClass('bubbledRight');
+
+      var height = 0;
+      $this.children().each(function(i, p){
+        height += $(p).outerHeight(true);
+      });
+
+      $this.css('height', height + 'px');
+    };
+
+    $('#scroller').on('iscrollcreated', function(){
+      //recalculateHeight($(this));
+    });
+
+    $('#scroller').on('iscrollscroll', function(ev, d){
+      console.log('scroll -> y: ', d.y, ' height: ', d.scrollerHeight);
+    });
+
+    $('#scroller').on('iscrollend', function(ev, d){
+      console.log('scrollend -> y: ', d.y, ' height: ', d.scrollerHeight);
+    });
+
+    $('#scroller').on('click', function(){
+      console.log('click -> ', arguments);
+    });
+
+    var count = 3;
+
+    $('#scroller').on('iscrollinfinite', function(ev, d) {
+      if(count<0) return;
+
+      window.setTimeout(function() {
+          count-=1;
+          $('#scroller div:eq(1)')
+            .append('<p class="bubbled' + (count % 2 === 0 ? 'Left' : 'Right') + '">This item was created at:<br/> ' + Date().toString() + '</p>');
+
+          d.deferred.resolve(); 
+        }, 5500);
+    });
+
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
